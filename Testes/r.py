39e146abcc0ec30ba46f2d664f07faeac7d4a7c6
JOSÉ VICTOR TEIXEIRA DA COSTA GAMA <jose.gama@live.estacio.br>
2019-01-11 15:59:18 -0300

    Adicionado arquivos

---
+++ Testes/r.py
@@ -0,0 +1,26 @@
+import requests, xlrd, scrapy
+from s import coletaDados
+
+def xls_parser(arquivo):
+    tabela = xlrd.open_workbook(arquivo).sheet_by_index(0)
+    qtd_linhas = tabela.nrows
+    linhas = []
+    for i in range(1, qtd_linhas):
+        linhas.append(
+            {
+                'prgcod': str(tabela.row(i)[0].value).replace('.0',''),
+                'co_no_uasg': str(tabela.row(i)[1].value).replace('.0',''),
+            }
+        )
+    return linhas
+
+
+if __name__ == "__main__":
+    arquivo = '/Users/JOSÉVICTORTEIXEIRADA/Documents/Projetos/DevTeste/Testes/Pregao_UASG.xls'
+    ld = xls_parser(arquivo)
+    url = 'http://comprasnet.gov.br/livre/pregao/termojulg.asp?prgcod=342018&Acao=A&co_no_uasg=70027'
+    lg = []
+    for i in range(0,len(ld)):
+        aux = url.replace('prgcod=342018', 'prgcod='+ld[i]['prgcod']).replace('co_no_uasg=70027', 'co_no_uasg='+ld[i]['co_no_uasg'])
+        lg.append(aux)
+        print(coletaDados(aux))
---
+++ Testes/s.py
@@ -0,0 +1,41 @@
+import requests, scrapy
+
+def coletaDados(url):
+    
+    response = requests.get(url)
+    sel = scrapy.selector
+    select = sel.Selector(response)
+    li = []
+    try:
+        tab = select.xpath('//tr//td//text()').extract()
+    
+        for i in range(0,len(tab)):
+            tab[i] = tab[i].replace('\n','').replace('\t','').replace('  ','').replace(': R$','').strip().rstrip(":").lstrip(', ')
+            if tab[i] == ' ':
+                tab[i] = tab[i].replace(' ','')
+            elif len(tab[i] ) != 0:
+                li.append(tab[i])
+
+        #Removendo lixo da lista e estrurando
+        del(li[0:10])
+
+        for x in range(0,2):
+            del(li[-1])
+
+        a = li.count('e a quantidade de')
+
+        for j in range(0, a):
+            while (j < a):
+                b = li.index('e a quantidade de')
+                del(li[b:b+4])
+
+        for x in range(0,len(li)):
+            if 'Fornecedor' in li[x]:
+                li[x] = li[x].replace('. ','\n').replace(': ','\n').replace(', ','\n').replace('Melhor lance ','Melhor lance\n').replace('Fornecedor:','Fornecedor\n').replace('CNPJ/CPF:','CNPJ/CPF\n').split('\n')
+            elif ',' in li[x]:
+                if 'R$' not in li[x]:
+                    aux = 'R$ ' + str(li[x])
+                li[x] = aux
+        return li
+    except:
+        pass
